4ebc9ce54e0c9a3e4f5a574ac8c3fe61a97c6677
Chris Renshaw <104800006+renners7777@users.noreply.github.com>
2023-02-19 14:56:41 +0000

    Solution for task 2

---
+++ ObjectsBasics4Tasks/Task2/app.js
@@ -0,0 +1,19 @@
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+// Should work like that:
+
+// let schedule = {};
+
+// alert(isEmpty(schedule)); // true
+
+// schedule["8:30"] = "get up";
+
+// alert(isEmpty(schedule)); // false
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        // if the loop has started, there is a property
+        return false;
+    }
+    return true;
+}
